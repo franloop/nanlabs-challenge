55edf7872e2ce631c746627c9a28604659107042
Franco Gimenez <franco.gimenez@osde.com.ar>
2022-11-14 23:24:53 -0300

    Init repo

---
+++ src/main/java/com/nanlab/challenge/spacex/taskmanagementapi/TaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.nanlab.challenge.spacex.taskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApiApplication.class, args);
+	}
+
+}
